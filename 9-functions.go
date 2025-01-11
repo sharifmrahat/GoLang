57689650f5deaafa79611a1046d4b499460bb776
sharifmrahat <sharifmrahat@gmail.com>
2025-01-11 23:37:04 +0600

    functions are added

---
+++ 9-functions.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func sayGreetings(name string){
+    fmt.Printf("Welcome here %v \n", name)
+}
+
+func sayBye(name string){
+    fmt.Printf("Goodbye %v \n", name)
+}
+
+func cycleNames(people []string, myFunc func(string)){
+    for _, value :=range people{
+        myFunc(value)
+    }
+}
+
+func circleAre(radius float64) float64 {
+   return math.Pi * radius * radius
+}
+
+func main(){
+    sayGreetings("Yahya")
+    sayBye("Yesa")
+
+    names := []string{"Rahim", "Karim", "Rihan", "Saad", "Muaz"}
+    cycleNames(names, sayGreetings)
+    cycleNames(names, sayBye)
+
+    area1 := circleAre(10.5)
+
+    area2 := circleAre(10.5)
+
+    fmt.Println(area1, area2)
+    fmt.Printf("Area of circle 1 is %0.3f", area1)
+    fmt.Printf("Area of circle 2 is %0.3f \n", area2)
+
+}
+
