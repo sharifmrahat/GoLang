bdbb4f2d7d43821d36a8479c389bbb548102c106
sharifmrahat <sharifmrahat@gmail.com>
2025-02-09 21:12:29 +0600

    tutorial-15/structs-and-types is added

---
+++ 15-custom-types.go
@@ -0,0 +1,28 @@
+package main
+
+// import "fmt"
+
+// //This is custom type or structure
+// type bill struct {
+// 	name string
+// 	items map[string]float64
+// 	tip float64
+// }
+
+// //make new bills
+// func newBill(name string) bill {
+// 	//initial bill object
+// 	yourBill := bill{
+// 		name: name,
+// 		items: map[string]float64{},
+// 		tip: 0,
+// 	}
+
+// 	return yourBill
+// }
+
+// func main() {
+//     myBill := newBill("John's Bill")
+//     fmt.Println(myBill)
+// }
+
